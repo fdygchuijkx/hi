069fea2d01d45662f39f324f4af6fefa929c5fa3
fdygchuijkx <ssjcanna.matteo@hotmail.com>
2024-05-09 21:15:00 +0700

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] > nums[right]) left = mid + 1;
+    else right = mid;
   }
-  return false;
+  return nums[left];
 }
