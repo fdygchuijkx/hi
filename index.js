2b8c4b51f8fa4656250fb8f3d4c6578f6ad08bf8
fdygchuijkx <ssjcanna.matteo@hotmail.com>
2024-05-12 04:01:45 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function partitionLabels(s) {
-  const last = new Array(26);
-  for (let i = 0; i < s.length; i++) {
-    last[s.charCodeAt(i) - "a".charCodeAt(0)] = i;
+const isPrime = (num) => {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) return false;
   }
-  let anchor = 0;
-  let j = 0;
-  const result = [];
-  for (let i = 0; i < s.length; i++) {
-    j = Math.max(j, last[s.charCodeAt(i) - "a".charCodeAt(0)]);
-    if (i === j) {
-      result.push(i - anchor + 1);
-      anchor = i + 1;
-    }
-  }
-  return result;
-}
+  return num > 1;
+};
